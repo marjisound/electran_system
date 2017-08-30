2e1c3862ac93d4458b4080a918426b683e2dee37
Marjan <marji_sound@yahoo.com>
2017-08-30 21:52:22 +0100

    test failour

--- electran_project/electran_project/settings.py
+++ electran_project/electran_project/settings.py
@@ -68,7 +68,7 @@ SECRET_KEY = '0tk%fl8%r574o6g_u)wx1rastm6%_#t10r_=yosftgrkk3_v3_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['electransystem.azurewebsites.net']
 
 
 # Application definition
