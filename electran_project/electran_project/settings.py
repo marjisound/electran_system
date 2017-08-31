9f02eaec274a526f72b5da2f29a331844695d16d
Marjan <marji_sound@yahoo.com>
2017-08-31 22:51:34 +0100

    static web config

--- electran_project/electran_project/settings.py
+++ electran_project/electran_project/settings.py
@@ -143,7 +143,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, 'my.cnf'),
+            'read_default_file': os.path.join(BASE_DIR, 'site.cnf'),
         },
     }
 }
